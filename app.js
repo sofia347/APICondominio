6b08a4ebec90e56e0ec49aaca910ed0a74c68081
Sofia Gonzalez <miroslava299064@gmail.com>
2024-12-05 19:57:35 -0600

    Modificar para el despliegue

--- app.js
+++ app.js
@@ -76,7 +76,8 @@ app.delete('/tasks/:id', (req, res) => {
 });
 
 // Inicia el servidor
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
